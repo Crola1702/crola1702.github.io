740a7e26e9b564de453ca2fe3056818fa49d0b02
Crola1702 <cristobal.arroyo@ekumenlabs.com>
2024-06-22 19:42:26 -0500

    Change site url
    Signed-off-by: Crola1702 <cristobal.arroyo@ekumenlabs.com>

--- publishconf.py
+++ publishconf.py
@@ -8,7 +8,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = "https://crola1702.github.io/crola1702-site/"
+SITEURL = "https://crola1702.github.io/"
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = "feeds/all.atom.xml"
